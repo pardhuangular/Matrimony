8579c8fff0110f19d7c19f233f26780908de372e
PARTHA SARADHI GAJULA <pardhuangular@gmail.com>
2021-02-13 21:20:54 -0800

    added nav and register

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  users:any;
+  constructor(private htpp:HttpClient) {}
+
+  ngOnInit(): void {}
+
+  registerToggle() {
+    this.registerMode = !this.registerMode;
+  }
+  getUsers() {
+    this.htpp.get('https://localhost:5001/api/users').subscribe(
+      (response) => {
+        this.users = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
